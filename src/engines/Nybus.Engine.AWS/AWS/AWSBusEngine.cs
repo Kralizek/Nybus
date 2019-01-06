24c537fcfd60bba27709efc7c6318209848918cf
Renato Golia <golia.renato@gmail.com>
2019-01-06 18:54:20 +0100

    Fixed AWSBusEngine

---
+++ src/engines/Nybus.Engine.AWS/AWS/AWSBusEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Microsoft.Extensions.Logging;
+
+namespace Nybus.AWS
+{
+    public class AWSBusEngine : IBusEngine
+    {
+        private readonly IAmazonSimpleNotificationService _sns;
+        private readonly IAmazonSQS _sqs;
+        private readonly ILogger<AWSBusEngine> _logger;
+
+        public AWSBusEngine(IAmazonSimpleNotificationService sns, IAmazonSQS sqs, ILogger<AWSBusEngine> logger)
+        {
+            _sns = sns ?? throw new ArgumentNullException(nameof(sns));
+            _sqs = sqs ?? throw new ArgumentNullException(nameof(sqs));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task NotifyFailAsync(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifySuccessAsync(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessageAsync(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IObservable<Message>> StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeToCommand<TCommand>() 
+            where TCommand : class, ICommand
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeToEvent<TEvent>() 
+            where TEvent : class, IEvent
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
