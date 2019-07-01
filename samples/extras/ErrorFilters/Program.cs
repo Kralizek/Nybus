fd14d4b7efb1d0ab2c2db53e9c76ed9d5ad225dc
Renato Golia <golia.renato@gmail.com>
2019-07-01 11:50:51 +0200

    Testing formatting with ReSharper

--- samples/extras/ErrorFilters/Program.cs
+++ samples/extras/ErrorFilters/Program.cs
@@ -40,7 +40,7 @@ namespace ErrorFilters
                 ["Nybus:CommandErrorFilters:0:type"] = "retry",
                 ["Nybus:CommandErrorFilters:0:maxRetries"] = Retries.ToString(),
                 ["Nybus:EventErrorFilters:0:type"] = "retry",
-                ["Nybus:EventErrorFilters:0:maxRetries"] = Retries.ToString(),
+                ["Nybus:EventErrorFilters:0:maxRetries"] = Retries.ToString()
             };
 
             var configurationBuilder = new ConfigurationBuilder();
@@ -49,34 +49,41 @@ namespace ErrorFilters
             var configuration = configurationBuilder.Build();
 
             var services = new ServiceCollection();
-            services.AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+            services.AddLogging(builder => builder.AddConsole()
+                                                  .SetMinimumLevel(LogLevel.Trace)
+                               );
 
             services.AddNybus(nybus =>
-            {
-                nybus.UseConfiguration(configuration, "Nybus");
+                              {
+                                  nybus.UseConfiguration(configuration, "Nybus");
 
-                nybus.UseRabbitMqBusEngine(rabbitMq =>
-                {
-                    rabbitMq.UseConfiguration("RabbitMq");
-                });
+                                  nybus.UseRabbitMqBusEngine(rabbitMq =>
+                                                             {
+                                                                 rabbitMq.UseConfiguration("RabbitMq");
+                                                             }
+                                                            );
 
-                nybus.SubscribeToCommand<TestCommand>(async (dispatcher, context) =>
-                {
-                    Console.WriteLine($"Processed command {context.Command.Message}");
-                    await dispatcher.RaiseEventAsync(new TestCommandReceived { Message = $@"Received message ""{context.Command.Message}""" });
+                                  nybus.SubscribeToCommand<TestCommand>(async (dispatcher, context) =>
+                                                                        {
+                                                                            Console.WriteLine($"Processed command {context.Command.Message}");
+                                                                            await dispatcher.RaiseEventAsync(new TestCommandReceived { Message = $@"Received message ""{context.Command.Message}""" });
 
-                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+                                                                            await Task.Delay(TimeSpan.FromMilliseconds(100));
 
-                    throw new Exception("Hello world");
-                });
+                                                                            throw new Exception("Hello world");
+                                                                        }
+                                                                       );
 
-                nybus.SubscribeToEvent<TestEvent>((d, msg) =>
-                {
-                    Console.WriteLine($"Processed event {msg.Event.Message}");
+                                  nybus.SubscribeToEvent<TestEvent>((d, msg) =>
+                                                                    {
+                                                                        Console.WriteLine($"Processed event {msg.Event.Message}");
 
-                    throw new Exception("Hello world");
-                });
-            });
+                                                                        throw new Exception("Hello world");
+                                                                    }
+                                                                   );
+                              }
+                             );
 
             var serviceProvider = services.BuildServiceProvider();
 
